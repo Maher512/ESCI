21dd53f1a79431de169a3e29a93dcb84630af39b
Maher512 <74532750+Maher512@users.noreply.github.com>
2023-05-04 17:00:42 +0300

    Update testscr.sh

--- testscr.sh
+++ testscr.sh
@@ -9,12 +9,12 @@ extracted_data_vpc=$(echo "$json_vpc" | jq -r '.Vpc.VpcId')
 # Create two subnets
 json_subnet1= $(aws ec2 create-subnet --vpc-id $extracted_data_vpc --cidr-block 10.11.1.0/24)
 
-json_subnet2= $(aws ec2 create-subnet --vpc-id $extracted_data_vpc --cidr-block 10.11.2.0/24)
+#json_subnet2= $(aws ec2 create-subnet --vpc-id $extracted_data_vpc --cidr-block 10.11.2.0/24)
 
 # Extract the subnet ID using jq
 extracted_data_subnet1=$(echo "$json_subnet1" | jq -r '.Subnet.SubnetId')
 
-extracted_data_subnet2=$(echo "$json_subnet2" | jq -r '.Subnet.SubnetId')
+#extracted_data_subnet2=$(echo "$json_subnet2" | jq -r '.Subnet.SubnetId')
 
 # Create an Internet Gateway
 json_igw=$(aws ec2 create-internet-gateway)
