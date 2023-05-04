1ae93095d24f10297c3219b2ba77ff04c8dacd50
Maher512 <74532750+Maher512@users.noreply.github.com>
2023-05-04 03:32:17 +0300

    Create testscr.sh

---
+++ testscr.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Replace this command with the one that produces JSON output
+json_output=$(aws ec2 create-vpc --cidr-block 10.11.0.0/16)
+
+# Extract the vpc ID using jq
+extracted_data=$(echo "$json_output" | jq -r '.Vpc.VpcId')
+
+aws ec2 create-subnet --vpc-id $extracted_data --cidr-block 10.11.1.0/24
