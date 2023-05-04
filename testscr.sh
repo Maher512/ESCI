0bf57e97db73455f0f485aff1f445f22575ca0f5
Maher512 <74532750+Maher512@users.noreply.github.com>
2023-05-04 15:21:22 +0300

    Update testscr.sh

--- testscr.sh
+++ testscr.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # Place wanted command that produces JSON output between the parentheses
-json_output=$(aws ec2 create-vpc --cidr-block 10.11.0.0/16)
+json_vpc=$(aws ec2 create-vpc --cidr-block 10.11.0.0/16)
 
 # Extract the vpc ID using jq
-extracted_data_vpc=$(echo "$json_output" | jq -r '.Vpc.VpcId')
+extracted_data_vpc=$(echo "$json_vpc" | jq -r '.Vpc.VpcId')
 
-aws ec2 create-subnet --vpc-id $extracted_data_vpc --cidr-block 10.11.1.0/24
-aws ec2 create-subnet --vpc-id $extracted_data_vpc --cidr-block 10.11.2.0/24
+# Create two subnets
+json_subnet1= $(aws ec2 create-subnet --vpc-id $extracted_data_vpc --cidr-block 10.11.1.0/24)
+sleep 2
+json_subnet2= $(aws ec2 create-subnet --vpc-id $extracted_data_vpc --cidr-block 10.11.2.0/24)
+
+# Extract the subnet ID using jq
+extracted_data_subnet1=$(echo "$json_subnet1" | jq -r '.Subnet.SubnetId')
+extracted_data_subnet2=$(echo "$json_subnet2" | jq -r '.Subnet.SubnetId')
 
 # Create an Internet Gateway
-aws ec2 create-internet-gateway
+#json_igw=$(aws ec2 create-internet-gateway)
