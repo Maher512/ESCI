19c1213dc36821942a78685d750116ff086db947
Maher512 <74532750+Maher512@users.noreply.github.com>
2023-05-17 14:38:38 +0300

    Update Infa_Creation.sh

--- Infa_Creation.sh
+++ Infa_Creation.sh
@@ -51,10 +51,10 @@ aws ec2 modify-subnet-attribute --subnet-id $extracted_data_subnet1 --map-public
 
 ##### COMMANDS BELOW LAUNCH AN EC2 INSTANCE INTO THE SUBNET FOR TESTING #####
 
-# Create a key pair and output to KCCpair.pem
-aws ec2 create-key-pair --key-name KCCpair.pem --query 'KeyMaterial' --output text > KCCpair.pem
+# Create a key pair and output to EPair.pem
+aws ec2 create-key-pair --key-name EPair.pem --query 'KeyMaterial' --output text > EPair.pem
 
-chmod 400 KCCpair.pem
+chmod 400 EPair.pem
 
 # Create a security group with a rule to allow ssh access
 json_sg=$(aws ec2 create-security-group --group-name FinSecurityGroup --description "Fintech 1.0 security group" --vpc-id $extracted_data_vpc)
@@ -66,7 +66,7 @@ extracted_data_sg=$(echo "$json_sg" | jq -r '.GroupId')
 aws ec2 authorize-security-group-ingress --group-id $extracted_data_sg --protocol tcp --port 22 --cidr 0.0.0.0/0
 
 # Launch an EC2 instance into the subnet
-json_insid=$(aws ec2 run-instances --image-id ami-02396cdd13e9a1257 --count 1 --instance-type t2.micro --key-name KCCpair.pem --security-group-ids $extracted_data_sg --subnet-id $extracted_data_subnet1)
+json_insid=$(aws ec2 run-instances --image-id ami-02396cdd13e9a1257 --count 1 --instance-type t2.micro --key-name EPair.pem --security-group-ids $extracted_data_sg --subnet-id $extracted_data_subnet1)
 
 # Get instance ID of the EC2 instance via jq
 extracted_data_insid=$(echo "$json_insid" | jq -r '.Groups[].Instances[].InstanceId')
@@ -81,6 +81,6 @@ json_IP=$(aws ec2 describe-instances --instance-ids $extracted_data_insid)
 extracted_data_IP=$(echo "$json_IP" | jq -r '.Reservations[].Instances[].PublicIpAddress')
 
 # SSH into the instance
-ssh -i KCCpair.pem ec2-user@$extracted_data_IP
+ssh -i EPair.pem ec2-user@$extracted_data_IP
 
 echo "Script complete"
