da338b58dba48b7262c2a615161ceb9df331b6fe
Maher512 <74532750+Maher512@users.noreply.github.com>
2023-05-17 16:46:32 +0300

    Update rolecreation.sh

--- rolecreation.sh
+++ rolecreation.sh
@@ -5,11 +5,11 @@ aws s3 mb s3://maher123-cli-bucket
 
 #Creating a Read Only Policy for the S3 Bucket
 
-json_arn_rp=$(aws iam create-policy --policy-name ReadOnlyS3Policy --policy-document file://s3bucket_read_only.json)
+json_arn_rp=$(aws iam create-policy --policy-name ReadOnlyS3Policy --policy-document file://Policies/s3bucket_read_only.json)
 
 extracted_arn=$(echo "$json_arn_rp" | jq -r '.Policy.Arn')
 
-aws iam create-role --role-name NUser --assume-role-policy-document file://trustpolicy.json
+aws iam create-role --role-name NUser --assume-role-policy-document file://Policies/trustpolicy.json
 
 aws iam attach-role-policy --role-name NUser --policy-arn $extracted_arn
 
@@ -19,11 +19,11 @@ sleep 2
 
 echo "Creating an Admin Role"
 
-json_arn_ap=$(aws iam create-policy --policy-name AdminPolicy --policy-document file://PermissionPolicy.json)
+json_arn_ap=$(aws iam create-policy --policy-name AdminPolicy --policy-document file://Policies/PermissionPolicy.json)
 
 extracted_arn_ap=$(echo "$json_arn_ap" | jq -r '.Policy.Arn')
 
-aws iam create-role --role-name Admin --assume-role-policy-document file://trustpolicy.json
+aws iam create-role --role-name Admin --assume-role-policy-document file://Policies/trustpolicy.json
 
 aws iam attach-role-policy --role-name Admin --policy-arn $extracted_arn_ap
 
