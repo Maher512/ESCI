4eec3b55106bc69862e4d0fb5c3d419d2a66812d
Maher512 <74532750+Maher512@users.noreply.github.com>
2023-05-17 12:45:39 +0300

    Creating roles

--- rolecreation.sh
+++ rolecreation.sh
@@ -3,3 +3,5 @@
 aws iam create-role --role-name MyEC2S3Role --assume-role-policy-document file://trustpolicy.json
 
 aws iam put-role-policy --role-name MyEC2S3Role --policy-name MyEC2S3Policy --policy-document file://PermissionsPolicy.json
+
+echo "Created role MyEC2S3Role with policy MyEC2S3Policy"
