e891e070a21008f3f4f4b2b89ff9b0fc9cd611a1
Maher512 <74532750+Maher512@users.noreply.github.com>
2023-05-17 13:05:19 +0300

    cleanup roles

--- rolecreation.sh
+++ rolecreation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Creating role MyEC2S3Role with policy MyEC2S3Policy"
+
 aws iam create-role --role-name MyEC2S3Role --assume-role-policy-document file://trustpolicy.json
 
 aws iam put-role-policy --role-name MyEC2S3Role --policy-name MyEC2S3Policy --policy-document file://PermissionPolicy.json
