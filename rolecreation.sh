9aea27f534074be1c1ee090b28e29230db242c41
Maher512 <74532750+Maher512@users.noreply.github.com>
2023-05-17 13:22:22 +0300

    rolecreation

--- rolecreation.sh
+++ rolecreation.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-echo "Creating role MyEC2S3Role with policy MyEC2S3Policy"
+echo "Creating an Admin Role"
 
-aws iam create-role --role-name MyEC2S3Role --assume-role-policy-document file://trustpolicy.json
+aws iam create-role --role-name Admin --assume-role-policy-document file://trustpolicy.json
 
-aws iam put-role-policy --role-name MyEC2S3Role --policy-name MyEC2S3Policy --policy-document file://PermissionPolicy.json
+aws iam put-role-policy --role-name Admin --policy-name AdminPolicy --policy-document file://PermissionPolicy.json
 
-echo "Created role MyEC2S3Role with policy MyEC2S3Policy"
+echo "Created an Admin Role"
+
+sleep 2
+
+echo "Creating an S3 Bucket and attaching it to a normal user role with limited permissions"
+
+aws s3 mb s3://maher123-cli-bucket
+
+echo "Created an S3 Bucket now creating the normal user role"
+
+aws iam create-role --role-name Norm-User --assume-role-policy-document file://s3bucket_read_only.json
+
+aws iam put-role-policy --role-name Norm-User --policy-name NormUserPolicy --policy-document file://ec2-ronly.json
